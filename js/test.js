9bcef97a77e265ec63c4056bed09d77490c94807
marc brooks <xaffimarc@gmail.com>
2018-05-20 13:41:59 +0100

    Exercise 1 Complete

---
+++ js/test.js
@@ -0,0 +1,5 @@
+/*
+    NOTE
+    I'm not sure why you want me to include this file, perhaps you intended me to write my css in there?
+    I've created the empty file just to avoid a 404 on the request, Apologies if i've missed the point here
+ */
